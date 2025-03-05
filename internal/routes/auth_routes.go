ba3336092597249c146ed20a105087e177bf20af
Lear0x <mathieu.lacroix.pro@gmail.com>
2025-03-05 15:42:03 +0100

    save route group

--- internal/routes/auth_routes.go
+++ internal/routes/auth_routes.go
@@ -7,9 +7,18 @@ import (
 )
 
 func SetupRoutes(router *gin.Engine) {
+
 	router.GET("/health", controllers.HealthCheck)
 	router.POST("/register", controllers.Register)
 	router.POST("/login", controllers.Login)
 	router.POST("/forgot-password", controllers.ForgotPassword)
 	router.POST("/reset-password", controllers.ResetPassword)
+
+	// 	api := router.Group("/19ebe88a-e0ce-42bc-8dcf-d5206d0658ad")
+
+	// 	api.GET("/health", controllers.HealthCheck)
+	// 	api.POST("/register", controllers.Register)
+	// 	api.POST("/login", controllers.Login)
+	// 	api.POST("/forgot-password", controllers.ForgotPassword)
+	// 	api.POST("/reset-password", controllers.ResetPassword)
 }
