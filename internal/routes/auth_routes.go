4c9a0b14c66f7a07773699229c966d90094d0bd7
Lear0x <mathieu.lacroix.pro@gmail.com>
2025-03-05 15:38:10 +0100

    forgot reset

--- internal/routes/auth_routes.go
+++ internal/routes/auth_routes.go
@@ -10,4 +10,6 @@ func SetupRoutes(router *gin.Engine) {
 	router.GET("/health", controllers.HealthCheck)
 	router.POST("/register", controllers.Register)
 	router.POST("/login", controllers.Login)
+	router.POST("/forgot-password", controllers.ForgotPassword)
+	router.POST("/reset-password", controllers.ResetPassword)
 }
